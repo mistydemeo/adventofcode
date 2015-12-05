cf83d02d438c2c8185b7965875d1cecbee393d5f
Misty De Meo <mistydemeo@gmail.com>
2015-12-05 13:41:09 -0800

    Day 4: add argument parsing to Ruby version

---
+++ 4/4.rb
@@ -0,0 +1,21 @@
+require "digest"
+
+INPUT = "bgvyzdsv"
+MD5 = Digest::MD5.new
+
+begin
+  prefix_size = ARGV[0].nil? ? 5 : Integer(ARGV[0])
+rescue ArgumentError
+  $stderr.puts "Argument was not an integer: #{ARGV[0]}"
+  exit 1
+end
+
+cmp_string = (["0"] * prefix_size).join
+
+i = 0
+begin
+  i += 1
+  digest = MD5.hexdigest INPUT + i.to_s
+end while digest[0...prefix_size] != cmp_string
+
+puts i
