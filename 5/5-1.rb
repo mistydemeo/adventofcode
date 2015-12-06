15df1060bfd25da45288b3b4066f7980d5b4f96b
Misty De Meo <mistydemeo@gmail.com>
2015-12-06 00:00:12 -0800

    Day 5: golf ruby

--- 5/5-1.rb
+++ 5/5-1.rb
@@ -2,14 +2,8 @@ FORBIDDEN = /(ab|cd|pq|xy)/
 VOWELS = /[aeiou]/
 REPEATED = /(.)\1/
 
-nice = []
-
 def nice? string
   !!((string =~ FORBIDDEN).nil? && string.scan(VOWELS).length >= 3 && string =~ REPEATED)
 end
 
-File.read("input.txt").chomp.split("\n").each do |line|
-  nice << line if nice? line
-end
-
-puts nice.length
+puts File.read("input.txt").chomp.split("\n").select(&method(:nice?)).length
--- 5/5-2.rb
+++ 5/5-2.rb
@@ -1,14 +1,8 @@
 REPEATED = /(..).*\1/
 SEPARATED = /(.).\1/
 
-nice = []
-
 def nice? string
   !!(string =~ REPEATED && string =~ SEPARATED)
 end
 
-File.read("input.txt").chomp.split("\n").each do |line|
-  nice << line if nice? line
-end
-
-puts nice.length
+puts File.read("input.txt").chomp.split("\n").select(&method(:nice?)).length
