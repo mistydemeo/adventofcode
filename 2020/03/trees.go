6be8d7a6330eea76d02f29b3a996238b339358a1
Misty De Meo <mistydemeo@gmail.com>
2020-12-30 21:21:19 -0800

    03: fix down step

--- 2020/03/trees.go
+++ 2020/03/trees.go
@@ -12,7 +12,7 @@ func RideSlope(right_step int, down_step int, lines []string) int {
 
 	trees := 0
 	step := 0
-	for i := 1; i < height; i++ {
+	for i := 1; i < height; i += down_step {
 		step++
 
 		// Borrowed from https://github.com/coingraham/adventofcode/blob/master/2020/day3.py#L35
